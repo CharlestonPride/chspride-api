53f44892c718039f0c7c0afeea5db189ed6446b8
Pete Bingel <pete.bingel@blackbaud.me>
2022-03-29 21:53:57 -0400

    Add ability to specify application env id Only send emails for prod

--- submitApplication/index.js
+++ submitApplication/index.js
@@ -8,28 +8,34 @@ module.exports = async function (context, req) {
 
   let id = crypto.randomUUID();
   req.body.id = id;
-  req.body.envId = "chspride";
+  if (!req.body.envId) {
+    req.body.envId = "chspride";
+  }
+
   req.body.status = "new";
+  req.body.submissionDate = new Date().toJSON();
 
   context.bindings.outputDocument = req.body;
 
   context.res = { body: { id } };
 
-  const sgMail = require("@sendgrid/mail");
-  sgMail.setApiKey(process.env.SendGridApiKey);
-  const boardMsg = {
-    to: `${process.env.SendGridApiTo}`,
-    from: `${process.env.SendGridApiFrom}`,
-    templateId: "d-b9c79d383f56451aaf6471c6368ff73a",
-    dynamicTemplateData: req.body,
-  };
-  sgMail.send(boardMsg);
+  if (req.body.envId === "chspride") {
+    const sgMail = require("@sendgrid/mail");
+    sgMail.setApiKey(process.env.SendGridApiKey);
+    const boardMsg = {
+      to: `${process.env.SendGridApiTo}`,
+      from: `${process.env.SendGridApiFrom}`,
+      templateId: "d-b9c79d383f56451aaf6471c6368ff73a",
+      dynamicTemplateData: req.body,
+    };
+    sgMail.send(boardMsg);
 
-  const applicantMsg = {
-    to: req.body.email,
-    from: `${process.env.SendGridApiFrom}`,
-    templateId: "d-5371de527a90403fad7a4dc5d4336e0e",
-    dynamicTemplateData: req.body,
-  };
-  sgMail.send(applicantMsg);
+    const applicantMsg = {
+      to: req.body.email,
+      from: `${process.env.SendGridApiFrom}`,
+      templateId: "d-5371de527a90403fad7a4dc5d4336e0e",
+      dynamicTemplateData: req.body,
+    };
+    sgMail.send(applicantMsg);
+  }
 };
